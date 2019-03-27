76967e69f9119822fbc88927f50d54a6b1b5d122
Eric Jefferson <ejefferson50@gmail.com>
2019-03-27 17:25:27 -0400

    Finished

--- src/main/java/com/zipcodewilmington/bakery/models/Baker.java
+++ src/main/java/com/zipcodewilmington/bakery/models/Baker.java
@@ -1,17 +1,23 @@
 package com.zipcodewilmington.bakery.models;
 
+
 import org.springframework.stereotype.Service;
 
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.Objects;
 
+@Entity
 public class Baker {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-
     private String name;
-
     private String employeeId;
-
     private String specialty;
 
     public Baker() {
